7e261b2dde2d38d662709355487ca627799d0e86
MadLicorice <madlicorice@gmail.com>
2018-02-28 21:42:39 -0800

    Change db connection for circleCI

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/weGotData');
+mongoose.connect('mongodb://localhost/weGotData');
 
 const restaurantSchema = mongoose.Schema({
   id: {
