c9d19afbe0eb084dee23e25ce96a7fcd06bee168
MadLicorice <madlicorice@gmail.com>
2018-04-19 21:02:10 -0700

    Change logo URL

--- client/src/components/WeGotDividerLine.jsx
+++ client/src/components/WeGotDividerLine.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DividerLine = () => (
   <div id="overview-divider-line">
     <hr id="overview-hline" />
-    <img id="overview-logo" src="http://res.cloudinary.com/madlicorice/image/upload/c_scale,q_100,w_46/v1520407902/WeGot_small_logo_recolored.png" alt="wegot-logo" />
+    <img id="overview-logo" src="http://res.cloudinary.com/madlicorice/image/upload/c_scale,q_100,w_46/v1524194328/WeGot_small_logo_recolored.png" alt="wegot-logo" />
   </div>
 );
 
